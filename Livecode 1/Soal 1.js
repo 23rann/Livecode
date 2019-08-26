71628b0bb93e1e1695a6c67ab337245eaa9cab51
23rann <53418684+23rann@users.noreply.github.com>
2019-08-26 16:02:10 +0700

    Add files via upload

---
+++ Livecode 1/Soal 1.js
@@ -0,0 +1,13 @@
+masukkan Nama 
+nilai Nilai 
+
+
+jika Nilai 80 sampai 100 = tampilkan A
+jika Nilai 65 sampai 79 = tampilkan B
+jika Nilai 50 sampai 64 = tampilkan C
+jika Nilai 35 sampai 49 = tampilkan D
+jika Nilai 0 sampai 34 = tampilkan E
+Jika Nilai dibawah 0 dan diatas 100 = tampilkan invalid
+
+
+ tampilkan  nama dan nilai
---
+++ Livecode 1/Soal 2.js
@@ -0,0 +1,8 @@
+if (name [0] == 'a' ||name [0] == 'b' ||name [0] == 'c' ||name [0] == 'd'){
+    console.log("Masuk grup pertama")
+}else if (name [0] == 'e' ||name [0] == 'f' ||name [0] == 'g' ||name [0] == 'h'){
+    console.log("Masuk grup kedua")
+}else {(console.log("Masuk grup terakhir"))
+}
+
+var name = " "
